a8cec1e65bdd6f27944e2fcc9cf28f5d68acbcef
shirish-pukale <158696162+shirish-pukale@users.noreply.github.com>
2025-04-26 15:35:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,69 +9,80 @@ resource "google_compute_address" "static_ip" {
   name = "secret-word-ip"
 }
 
-# Create SSL Certificate for Load Balancer
-resource "google_compute_ssl_certificate" "self_signed_cert" {
-  name = "self-signed-cert"
-  private_key = file("certs/privkey.pem")
-  certificate = file("certs/fullchain.pem")
+# Use managed SSL certificate instead of missing PEM files
+resource "google_compute_managed_ssl_certificate" "ssl_cert" {
+  name = "managed-cert"
+
+  managed {
+    domains = [var.domain_name]
+  }
 }
 
-# Create a health check for Load Balancer
+# Health check for Load Balancer
 resource "google_compute_http_health_check" "http_health_check" {
-  name = "http-health-check"
+  name         = "http-health-check"
   request_path = "/"
-  port = 3000
+  port         = 3000
 }
 
-# Create a backend service for the Load Balancer
+# Backend service
 resource "google_compute_backend_service" "backend_service" {
   name        = "backend-service"
   protocol    = "HTTP"
   timeout_sec = 10
 
-  backends {
+  backend {
     group = google_compute_instance_group.app_instance_group.self_link
   }
 
   health_checks = [google_compute_http_health_check.http_health_check.self_link]
 }
 
-# Create a URL map for the Load Balancer
+# URL map
 resource "google_compute_url_map" "url_map" {
-  name = "url-map"
-
-  default_backend_service = google_compute_backend_service.backend_service.self_link
+  name            = "url-map"
+  default_service = google_compute_backend_service.backend_service.self_link
 }
 
-# Create an HTTPS proxy for the Load Balancer
+# HTTPS proxy using managed SSL certificate
 resource "google_compute_target_https_proxy" "https_proxy" {
   name             = "https-proxy"
-  ssl_certificates = [google_compute_ssl_certificate.self_signed_cert.self_link]
+  ssl_certificates = [google_compute_managed_ssl_certificate.ssl_cert.self_link]
   url_map          = google_compute_url_map.url_map.self_link
 }
 
-# Create a global forwarding rule for the Load Balancer
+# Global forwarding rule
 resource "google_compute_global_forwarding_rule" "https_forwarding_rule" {
-  name        = "https-forwarding-rule"
-  target      = google_compute_target_https_proxy.https_proxy.self_link
-  port_range  = "443"
-  ip_address  = google_compute_address.static_ip.address
+  name       = "https-forwarding-rule"
+  target     = google_compute_target_https_proxy.https_proxy.self_link
+  port_range = "443"
+  ip_address = google_compute_address.static_ip.address
 }
 
-# Create an instance group to hold VM instances
+# Instance group
 resource "google_compute_instance_group" "app_instance_group" {
-  name        = "app-instance-group"
-  zone        = var.zone
-  instances   = [google_compute_instance.app_instance.self_link]
+  name      = "app-instance-group"
+  zone      = var.zone
+  instances = [google_compute_instance.app_instance.self_link]
 }
 
-# Create a VM instance to run Docker containers
+# VM Instance
 resource "google_compute_instance" "app_instance" {
   name         = "secret-word-instance"
   machine_type = "e2-medium"
   zone         = var.zone
-  image        = "projects/debian-cloud/global/images/family/debian-10"
   tags         = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/family/debian-10"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
@@ -83,7 +94,16 @@ resource "google_compute_instance" "app_instance" {
   EOF
 }
 
-# Output the Load Balancer IP
-output "load_balancer_ip" {
-  value = google_compute_address.static_ip.address
+# Firewall rule with source_ranges and correct tags
+resource "google_compute_firewall" "allow_http_https" {
+  name    = "allow-http-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server", "https-server"]
 }
